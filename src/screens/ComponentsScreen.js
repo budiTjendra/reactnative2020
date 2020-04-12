054f53cb2b316f53f9e15a0cdf094fe0f285401a
budi-injo <budi.injo@telepathy.ai>
2020-04-12 16:03:43 +0800

    create component called componentsScreen

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,15 @@
+import react from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+const ComponentScreen = () => {
+  return <Text style={styles.textStyle}>This is component screen</Text>
+};
+
+const styles = StyleSheet.Create({
+    textStyle: {
+        fontSize:30
+    }
+
+})
+
+export default ComponentScreen
